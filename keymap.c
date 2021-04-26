1335742afa33c33c68e281f33cd3fdd44cd35745
Nils K <git@nkn-it.de>
2021-04-26 23:17:28 +0200

    mouse button

--- keymap.c
+++ keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC,   KC_RBRC, KC_BSLS, KC_DEL,  KC_END,            KC_PGDN,
         TG(_ML), KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,                            KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,               KC_SPC,                       KC_RALT, TT(_FL), KC_RCTL,   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,               KC_SPC,                       KC_RALT, TT(_FL), KC_BTN2,   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FL] = LAYOUT(
         KC_SLEP, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,           _______, _______, KC_WAKE,
